168a5d42ba8c8219840825b8358da9224cfdc1b0
Squishy123 <christian.gnaw@gmail.com>
2019-01-07 17:40:04 -0500

    Added toggler for logo on nav for scroll event

--- js/main.js
+++ js/main.js
@@ -10,10 +10,23 @@ window.addEventListener('scroll', (e) => {
             //change opacity of nav
             let nav = document.querySelector('.nav');
 
+            let nationalLogo = document.querySelector('#national-logo');
+
+            let campusLogo = document.querySelector('#campus-logo');
+
+
             if(scroll_position != 0) {
                 nav.classList.replace("nav-trans", "nav-solid");
+
+                nationalLogo.classList.remove('hidden');
+
+                campusLogo.classList.add('hidden');
             } else {
                 nav.classList.replace("nav-solid", "nav-trans");
+
+                nationalLogo.classList.add('hidden');
+
+                campusLogo.classList.remove('hidden');
             }
 
             ticking = false;
