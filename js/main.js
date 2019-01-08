5583eb1c725496782746be96fbe39e8d70fc0a6a
Squishy123 <christian.gnaw@gmail.com>
2019-01-07 19:42:07 -0500

    Revert "Revert "Added WIP snap scrolling""
    This reverts commit 38c656c7a8427ac44bf61e981b46124d8ff0ea9d.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,44 @@
 let scroll_position = 0;
+let scrolling = false;
+
 let ticking = false;
+
+//manage scroll targets
+let targets = ["#panel1", "#panel2", "#panel3"];
+targets = targets.map((t) => {
+    let targ = document.querySelector(t);
+    return {
+        pos: targ.offsetTop,
+        sel: t
+    };
+});
 
 //manage scroll
 window.addEventListener('scroll', (e) => {
-    scroll_position = window.scrollY;
+    let snappedPos = null;
 
-    if(!ticking) {
+    if (!scrolling) {
+        //snap scroll to target
+        if (scroll_position > window.scrollY) {
+            snappedPos = targets.reverse().find((e) => {
+                return window.scrollY > e.pos;
+            });
+            scrolling = true;
+
+        } else {
+            scrolling = false;
+        }
+    }
+
+    if (scrolling == true && snappedPos != null) {
+        console.log("Scrolling");
+        scrolling = false;
+        location.href = snappedPos.sel;
+    }
+
+    scroll_position = window.scrollY;
+    //console.log(scroll_position);
+    if (!ticking) {
         window.requestAnimationFrame(() => {
             //change opacity of nav
             let nav = document.querySelector('.nav');
@@ -15,7 +48,7 @@ window.addEventListener('scroll', (e) => {
             let campusLogo = document.querySelector('#campus-logo');
 
 
-            if(scroll_position != 0) {
+            if (scroll_position != 0) {
                 nav.classList.replace("nav-trans", "nav-solid");
 
                 nationalLogo.classList.remove('hidden');
