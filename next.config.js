d4ab287d3fece78ea57b73b38a38ca747d968f9a
Squishy123 <christian.gnaw@gmail.com>
2019-03-03 11:39:48 -0500

    Added scss and next-compose

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withPlugins = require('next-compose-plugins');
+
+const sass = require('@zeit/next-sass');
+
+module.exports = withPlugins([
+    [sass, {
+        cssModules: true
+    }]
+]);
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import style from './index.scss';
+
 export default () => (
     <div><h1>Hello World!</h1></div>
 );
