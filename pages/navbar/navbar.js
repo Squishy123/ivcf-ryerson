5d2159f25c15ef4afd804d954743d847f46a7966
Squishy123 <christian.gnaw@gmail.com>
2019-03-03 19:25:06 -0500

    Added working nav toggler and new media mixins

--- pages/navbar/navbar.js
+++ pages/navbar/navbar.js
@@ -5,6 +5,16 @@ import Link from 'next/link';
 export default class Navbar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            menuExpanded: false
+        };
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu() {
+        this.setState({ menuExpanded: !this.state.menuExpanded });
     }
 
     render() {
@@ -14,13 +24,18 @@ export default class Navbar extends React.Component {
                     <div className={styles.logoContainer}>
                         <img src={this.props.logoSRC} className={styles.logo}/>
                     </div>
-                    <div className={styles.pages}>
+                    <div className={`${styles.pages} ${(this.state.menuExpanded) ? styles.pagesExpanded : ""}`}>
                         {this.props.pages.map((p) => (
                             <Link href={p.href} key={p.name}>
                                 <p className={styles.navbarLink}>{p.name}</p>
                             </Link>
                         ))}
                     </div>
+                </div>
+                <div className={`${styles.toggler} ${(this.state.menuExpanded) ? styles.menuExpand : ""}`} onClick={this.toggleMenu}>
+                    <div className={styles.bar1}/>
+                    <div className={styles.bar2}/>
+                    <div className={styles.bar3}/>
                 </div>
             </div>
         )
