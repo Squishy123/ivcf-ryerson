b06c1a11ac8023add1f01061c89b8530b7547fcc
Squishy123 <christian.gnaw@gmail.com>
2019-03-05 18:00:39 -0500

    Added favicon

--- pages/pagehead/pagehead.js
+++ pages/pagehead/pagehead.js
@@ -13,7 +13,7 @@ export default class PageHead extends React.Component {
                 <title>IVCF | Ryerson University</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-                <link rel="icon" href="res/favicon.png"/>
+                <link rel="icon" href="/static/favicon.png"/>
 
                 <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
                 <link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" rel="stylesheet"/>
